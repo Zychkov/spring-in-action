8a4ace27135944162500fe5b9fd20533fe4d4127
azychkov <Artem.Zychkov@t-systems.com>
2022-06-19 12:23:29 +0300

    jdbctemplate is implemented

--- src/main/java/ru/zychkov/tacocloud/Taco.java
+++ src/main/java/ru/zychkov/tacocloud/Taco.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,5 +20,9 @@ public class Taco {
 
     @NotNull
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientRef> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient taco) {
+        this.ingredients.add(new IngredientRef(taco.getId()));
+    }
 }
